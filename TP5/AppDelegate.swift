124f018a4b39e5331d96a90ca93c19b97ccae7c4
GuillaumeSavin <guisav1996@hotmail.fr>
2020-10-12 09:18:05 +0200

    Initial Commit

---
+++ TP5/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TP5
+//
+//  Created by sg812131 on 12/10/2020.
+//  Copyright © 2020 sg812131_savin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
